a93afb0928f54ffa9e63940b5789fc7fef8fe76d
maksymnure <maksym.iziumskyi@nure.ua>
2022-08-22 02:09:02 +0300

    SRC-11 Add Registration page

---
+++ WebApplication3/StartPage.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="StartPage.aspx.cs" Inherits="WebApplication3.StartPage" %>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+     <title>Главная</title>
+    <link rel="stylesheet" href="StartPage.css"/>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <header>
+        <nav>
+            <asp:LinkButton ID="StartPager" runat="server" onclick="StartPager_Click" CssClass="LinkButton">Головна</asp:LinkButton>
+            <asp:LinkButton ID="RegisterPage" runat="server" onclick="RegisterPage_Click" CssClass="LinkButton">Зареєструватися</asp:LinkButton>
+        </nav>
+    </header>
+        <div class="form">
+            <h2>Login</h2>
+                <div class="inputBox">
+                    <label>Username</label><br/>
+                    <asp:TextBox ID="TextBoxLogin" runat="server" CssClass="InputArea"></asp:TextBox>
+                </div>
+                <div class="inputBox">
+                    <label>Password</label><br/>
+                    <asp:TextBox ID="TextBoxPassword" runat="server" TextMode="Password" CssClass="InputArea">Klass</asp:TextBox>
+                </div>
+            <div class="ButtomInput">
+                <asp:Button ID="SignIn" runat="server" Text="Вход" onclick="SignIn_Click" CssClass="Button"/>
+            </div>
+            <div class="divProblem">
+                <asp:Label ID="Label1ms" runat="server" CssClass="Problem"></asp:Label>
+            </div>
+      </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication3/StartPage.aspx.designer.cs
@@ -0,0 +1,80 @@
+﻿//------------------------------------------------------------------------------
+// <автоматически создаваемое>
+//     Этот код создан программой.
+//
+//     Изменения в этом файле могут привести к неправильной работе и будут потеряны в случае
+//     повторной генерации кода. 
+// </автоматически создаваемое>
+//------------------------------------------------------------------------------
+
+namespace WebApplication3
+{
+
+
+    public partial class StartPage
+    {
+
+        /// <summary>
+        /// form1 элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически созданное поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// StartPager элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически созданное поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton StartPager;
+
+        /// <summary>
+        /// RegisterPage элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически созданное поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton RegisterPage;
+
+        /// <summary>
+        /// TextBoxLogin элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически созданное поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBoxLogin;
+
+        /// <summary>
+        /// TextBoxPassword элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически созданное поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBoxPassword;
+
+        /// <summary>
+        /// SignIn элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически созданное поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button SignIn;
+
+        /// <summary>
+        /// Label1ms элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически созданное поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1ms;
+    }
+}
